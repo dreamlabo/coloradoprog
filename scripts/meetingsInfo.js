b86a0cd98613c9db59c6d2c5524860f9fad1264f
dreamlabo <toddlabo@gmail.com>
2024-07-17 12:11:03 -0600

    Added in new august meeting info, and reformatted strings to constants in the meeting scripts file.

--- scripts/meetingsInfo.js
+++ scripts/meetingsInfo.js
@@ -1,23 +1,20 @@
+const KENTS_EMAIL = 'kent.adamson@comcast.net'
+const KENTS_HOST_NAME = 'Kent'
+const MEETING_TIME_6PM = '6:00pm'
+
 meetings = [
     {
-        date: new Date('2024/3/30'),
+        date: new Date('2024/4/30'),
         time: '6:00pm',
         host: 'Kent',
         hostEmail: 'kent.adamson@comcast.net',
         additionalInfo: []
     },
     {
-        date: new Date('2023/11/11'),
-        time: '6:00pm',
-        host: 'Kent',
-        hostEmail: 'kentsEmail@mail.com',
-        additionalInfo: []
-    },
-    {
-        date: new Date('2024/08/10'),
-        time: '6:00pm',
-        host: 'Kent',
-        hostEmail: 'kent@mail.com',
+        date: new Date('2024/08/03'),
+        time: MEETING_TIME_6PM ,
+        host: KENTS_HOST_NAME,
+        hostEmail: KENTS_EMAIL,
         additionalInfo: []
     }, 
 ]
@@ -26,11 +23,11 @@ function returnUpcomingMeeting() {
 
     var currentDate = new Date(new Date().toDateString()); 
     
-    const currentDateFormatted = currentDate.toLocaleDateString('en-US', {  timeZone: 'America/Denver' });
+    // const currentDateFormatted = currentDate.toLocaleDateString('en-US', {  timeZone: 'America/Denver' });
     
     return meetings.find(meeting => {
         var date2 = new Date(new Date(meeting.date).toDateString()); 
-        const meetingDateFormatted = meeting.date.toLocaleDateString('en-US', {  timeZone: 'America/Denver' });
+        // const meetingDateFormatted = meeting.date.toLocaleDateString('en-US', {  timeZone: 'America/Denver' });
         return date2 >= currentDate;
       });
 }
