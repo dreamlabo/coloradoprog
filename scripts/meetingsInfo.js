03bc8f84975eeed6d6bf9340edb179ef49a4e366
dreamlabo <toddlabo@gmail.com>
2025-09-16 21:09:04 -0600

    Update: Updated the Sept 2025 meeting info.

--- scripts/meetingsInfo.js
+++ scripts/meetingsInfo.js
@@ -5,53 +5,6 @@ const MEETING_TIME_5PM = "5:00pm";
 
 meetings = [
   {
-    date: new Date("2024/08/31"),
-    time: MEETING_TIME_5PM,
-    host: KENTS_HOST_NAME,
-    hostEmail: KENTS_EMAIL,
-    additionalInfo: [
-      "Please note the earlier start time.",
-      "Using the proceeds from our annual Progfest, food from GQue Barbecue, including ribs, chicken, brisket, coleslaw, beans, buns & sauces, will be provided.",
-      "Please bring your own beverages, and if you'd like, feel free to bring a dessert (or side dish) to share!",
-      "Hope to see you there!",
-    ],
-  },
-  {
-    date: new Date("2024/12/21"),
-    time: MEETING_TIME_5PM,
-    host: KENTS_HOST_NAME,
-    hostEmail: KENTS_EMAIL,
-    additionalInfo: ["Please note the earlier start time."],
-  },
-  {
-    date: new Date("2025/03/01"),
-    time: MEETING_TIME_6PM,
-    host: KENTS_HOST_NAME,
-    hostEmail: KENTS_EMAIL,
-    additionalInfo: [],
-  },
-  {
-    date: new Date("2025/05/03"),
-    time: MEETING_TIME_6PM,
-    host: KENTS_HOST_NAME,
-    hostEmail: KENTS_EMAIL,
-    additionalInfo: [],
-  },
-  {
-    date: new Date("2025/06/07"),
-    time: MEETING_TIME_6PM,
-    host: KENTS_HOST_NAME,
-    hostEmail: KENTS_EMAIL,
-    additionalInfo: [],
-  },
-  {
-    date: new Date("2025/07/12"),
-    time: MEETING_TIME_6PM,
-    host: KENTS_HOST_NAME,
-    hostEmail: KENTS_EMAIL,
-    additionalInfo: [],
-  },
-  {
     date: new Date("2025/08/23"),
     time: MEETING_TIME_5PM,
     host: KENTS_HOST_NAME,
@@ -64,6 +17,15 @@ meetings = [
       "Hope to see you there!",
     ],
   },
+    {
+    date: new Date("2025/10/04"),
+    time: MEETING_TIME_6PM,
+    host: KENTS_HOST_NAME,
+    hostEmail: KENTS_EMAIL,
+    additionalInfo: [],
+  },
+
+
 ];
 
 function returnUpcomingMeeting() {
